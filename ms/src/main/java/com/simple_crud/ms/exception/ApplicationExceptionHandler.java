686ea833dc4cff3cfb2ba873585cb2acc22a1784
Erinaldo Souza <erinaldo.sds@gmail.com>
2025-03-31 09:10:11 -0300

    exception handler improvements

--- ms/src/main/java/com/simple_crud/ms/exception/ApplicationExceptionHandler.java
+++ ms/src/main/java/com/simple_crud/ms/exception/ApplicationExceptionHandler.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.MissingFormatArgumentException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -15,7 +17,7 @@ public class ApplicationExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationExceptionHandler.class);
 
-    private static final String DEFAULT_ERROR_TITLE = "An error occured";
+    private static final String DEFAULT_ERROR_TITLE = "An error occurred";
     private static final String DEFAULT_ERROR_MESSAGE = "Try again later";
     private static final String NOT_FOUND_ERROR_TITLE = "Not found";
     private static final String NOT_FOUND_ERROR_MESSAGE = "The specified device does not exist";
@@ -38,6 +40,16 @@ public class ApplicationExceptionHandler {
                 new ErrorResponse(NOT_FOUND_ERROR_TITLE, NOT_FOUND_ERROR_MESSAGE)
         );
     }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        logIt(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ErrorResponse(DEFAULT_ERROR_TITLE, e.getBody().getDetail())
+        );
+    }
+
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
